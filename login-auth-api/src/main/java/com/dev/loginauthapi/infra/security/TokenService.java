d732d54d1df828a89d9136d777fee76eb8664a30
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-10-17 12:54:32 -0300

    feat: CustomUserDetailsService, SecurityConfig, SecurityFilter, TokenService, UserRepository

--- login-auth-api/src/main/java/com/dev/loginauthapi/infra/security/TokenService.java
+++ login-auth-api/src/main/java/com/dev/loginauthapi/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.dev.loginauthapi.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.dev.loginauthapi.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,20 @@ public class TokenService {
             return token;
         }catch (JWTCreationException e){
             throw  new RuntimeException("Error while authentication!");
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return  JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return null;
         }
     }
 
--- login-auth-api/src/main/java/com/dev/loginauthapi/repository/UserRepository.java
+++ login-auth-api/src/main/java/com/dev/loginauthapi/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.dev.loginauthapi.repository;
 
 import com.dev.loginauthapi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String login);
 }
